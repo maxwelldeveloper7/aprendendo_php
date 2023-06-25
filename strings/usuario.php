45bb0b8b972012ab3ebd89d41e874d3ef4ece8c3
maxwelldeveloper7 <maxwellchaves1844@gmail.com>
2023-06-25 20:12:41 -0300

    atualizando mb

--- strings/usuario.php
+++ strings/usuario.php
@@ -13,7 +13,7 @@ $posicaoDoArroba = strpos($email, '@');
 
 $usuario = substr($email, 0, $posicaoDoArroba);
 
-echo strtoupper($usuario) . PHP_EOL;
+echo mb_strtoupper($usuario) . PHP_EOL;
 echo substr($email, $posicaoDoArroba + 1) . PHP_EOL;
 
 list($nome, $sobrenome) = explode(' ', $nome);
