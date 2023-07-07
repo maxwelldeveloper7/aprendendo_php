79fc3d1e834c6c6dcaacbc5959d5e16974239345
maxwelldeveloper7 <maxwellchaves1844@gmail.com>
2023-07-07 17:00:15 -0300

    aula 03

--- orientacao_objetos/src/Conta.php
+++ orientacao_objetos/src/Conta.php
@@ -2,7 +2,35 @@
 
 class Conta
 {
-    public string $cpfTitular;
-    public string $nomeTitular;
-    public float $saldo;
+    public $cpfTitular;
+    public $nomeTitular;
+    public $saldo = 0;
+
+    public function sacar(float $valorASacar)
+    {
+        if ($valorASacar > $this->saldo){
+            echo "Saldo indisponível";
+            return;
+        } 
+        $this->saldo -= $valorASacar;
+    }
+
+    public function depositar(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+            return;
+        }
+        $this->saldo += $valorADepositar;
+    }
+
+    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    {
+        if($valorATransferir > $this->saldo){
+            echo "saldo indisponível";
+            return;
+        }
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
+    }
 }
